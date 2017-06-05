4f7a0b7c08a0e6ad37929180e0bb005ffb83483a
tupengxiong <511980432@qq.com>
2017-06-05 16:03:23 +0800

    修改配置错误

--- dubbo-spring-zk/dubbo-spring-zk-provider/src/test/java/provider/test/JunitTestProvider.java
+++ dubbo-spring-zk/dubbo-spring-zk-provider/src/test/java/provider/test/JunitTestProvider.java
@@ -3,19 +3,15 @@ package provider.test;
 import java.io.IOException;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JunitTestProvider {
 
-	/*@Test
+	@Test
 	public void run() throws IOException{
-		ApplicationContext cxt = new ClassPathXmlApplicationContext("applicationContext.xml");
-		System.in.read();
-	}*/
-	
-	public static void main(String[] args) throws IOException {
-		ApplicationContext cxt = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
+		@SuppressWarnings("resource")
+		ClassPathXmlApplicationContext  cxt = new ClassPathXmlApplicationContext("applicationContext.xml");
+		cxt.start();
 		System.in.read();
 	}
 }
