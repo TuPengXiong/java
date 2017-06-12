a1acc20fe0ccd7b46c60bb856f5cb1b3da15bd36
tupengxiong <511980432@qq.com>
2017-06-12 11:30:07 +0800

    对接图灵接口

--- weixin/src/main/java/com/tupengxiong/weixin/controller/TestController.java
+++ weixin/src/main/java/com/tupengxiong/weixin/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.tupengxiong.weixin.controller;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -8,7 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tupengxiong.weixin.bean.Sn;
 import com.tupengxiong.weixin.bean.WxText;
+import com.tupengxiong.weixin.mapper.SnMapper;
 import com.tupengxiong.weixin.utils.TuLingTools;
 
 @Service
@@ -17,7 +20,9 @@ public class TestController {
 
 	@Resource
 	TuLingTools tuLingTools;
-
+	@Resource
+	private SnMapper snMapper;
+	
 	@RequestMapping("/test")
 	public void test(HttpServletResponse response,String msg) throws IOException {
 		WxText wxText = new WxText();
@@ -31,6 +36,16 @@ public class TestController {
 		wxText.setContent(content);
 		String resp = tuLingTools.getWxResp(wxText);
 		response.getWriter().write(resp);
+		response.getWriter().flush();
+		response.getWriter().close();
+	}
+	
+	@RequestMapping("/sn")
+	public void sn(HttpServletResponse response,String msg) throws IOException {
+		Sn sn = new Sn();
+		sn.setCreateTime(new Date().getTime());
+		snMapper.insert(sn);
+		response.getWriter().write("success");
 		response.getWriter().flush();
 		response.getWriter().close();
 	}
