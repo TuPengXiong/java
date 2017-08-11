0ee64ae576b708f8138b28081eb0940dd2f6ac09
tupengxiong <511980432@qq.com>
2017-08-11 10:08:30 +0800

    spring-cloud-config

---
+++ spring-cloud/cloud-config/src/main/java/com/tpx/spring/cloud/config/Application.java
@@ -0,0 +1,24 @@
+package com.tpx.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tpx on 2017/8/11.
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
