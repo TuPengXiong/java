e852b8ef95c0665ec41e6e55a26e7aa4305e0077
tupengxiong <511980432@qq.com>
2017-11-11 09:48:46 +0800

    util

--- weixin/src/test/java/com/tupengxiong/weixin/AppTest.java
+++ weixin/src/test/java/com/tupengxiong/weixin/AppTest.java
@@ -1,5 +1,13 @@
 package com.tupengxiong.weixin;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.apache.http.HttpResponse;
+
+import com.tupengxiong.weixin.utils.HttpUtils;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -35,4 +43,29 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+	public static void main(String[] args) {
+	    String host = "http://sms.market.alicloudapi.com";
+	    String path = "/singleSendSms";
+	    String method = "GET";
+	    String appcode = "你自己的AppCode";
+	    Map<String, String> headers = new HashMap<String, String>();
+	    //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+	    headers.put("Authorization", "APPCODE " + appcode);
+	    Map<String, String> querys = new HashMap<String, String>();
+	    querys.put("ParamString", "{\"no\":\"123456\"}");
+	    querys.put("RecNum", "RecNum");
+	    querys.put("SignName", "SignName");
+	    querys.put("TemplateCode", "TemplateCode");
+
+
+	    try {
+	    	HttpResponse response = HttpUtils.doGet(host, path, method, headers, querys);
+	    	System.out.println(response.toString());
+	    	//获取response的body
+	    	//System.out.println(EntityUtils.toString(response.getEntity()));
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+	}
 }
