a560e7567445396ca2eb3ca942a90b19ac160a1b
tupengxiong <511980432@qq.com>
2017-11-23 10:54:25 +0800

    netty

--- netty-master/src/main/java/com/lovesher/netty_master/http/HttpServerInboundHandler.java
+++ netty-master/src/main/java/com/lovesher/netty_master/http/HttpServerInboundHandler.java
@@ -1,5 +1,7 @@
 package com.lovesher.netty_master.http;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import io.netty.buffer.ByteBuf;
@@ -8,13 +10,13 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
-import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.util.CharsetUtil;
 
 public class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
@@ -31,9 +33,51 @@ public class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if (msg instanceof HttpRequest) {
+		if (msg instanceof FullHttpRequest) {
+			FullHttpRequest request = (FullHttpRequest) msg;
+			HttpMethod method = request.getMethod();
+			HttpVersion httpVersion = request.getProtocolVersion();
+			System.out.println("httpVersion>>>>>>>>>>>>>>" + httpVersion.text());
+			System.out.println("method>>>>>>>>>>>>>>" + method.name());
+			System.out.println("Uri>>>>>>>>>>>>>>" + request.getUri());
+			ByteBuf content = request.content();
+			final StringBuilder contentSb = new StringBuilder();
+			contentSb.append(content.toString(CharsetUtil.UTF_8));
+			System.out.println("content>>>>>>>>>>>>>>" + contentSb.toString());
+			QueryStringDecoder decoderQuery = new QueryStringDecoder(request.getUri());
+			Map<String, List<String>> uriAttributes = decoderQuery.parameters();
+			for (Map.Entry<String, List<String>> attr : uriAttributes.entrySet()) {
+				System.out.println(attr.getKey() + ">>>>>>>>>>>>>>>>>" + attr.getValue());
+			}
+			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+			// Thread.sleep(10000L);
+			response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+			int i = httpReceiveTotal.incrementAndGet();
+			// 转发
+			String responseMessage = "false============>" + i + ">>>>" + contentSb.toString();
+			ByteBuf buffer = Unpooled.copiedBuffer(responseMessage, CharsetUtil.UTF_8);
+			response.content().writeBytes(buffer);
+			// 释放缓冲区
+			buffer.release();
+			// 返回数据
+			ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+
+		}
+		
+		/*if (msg instanceof HttpRequest) {
 			HttpRequest request = (HttpRequest) msg;
-			System.out.println(request.getUri());
+			HttpMethod method = request.getMethod();
+			HttpVersion httpVersion = request.getProtocolVersion();
+			System.out.println("httpVersion>>>>>>>>>>>>>>" + httpVersion.text());
+			System.out.println("method>>>>>>>>>>>>>>" + method.name());
+			System.out.println("Uri>>>>>>>>>>>>>>" + request.getUri());
+			final StringBuilder contentSb = new StringBuilder();
+			System.out.println("content>>>>>>>>>>>>>>" + contentSb.toString());
+			QueryStringDecoder decoderQuery = new QueryStringDecoder(request.getUri());
+			Map<String, List<String>> uriAttributes = decoderQuery.parameters();
+			for (Map.Entry<String, List<String>> attr : uriAttributes.entrySet()) {
+				System.out.println(attr.getKey() + ">>>>>>>>>>>>>>>>>" + attr.getValue());
+			}
 		}
 		if (msg instanceof LastHttpContent) {
 			HttpContent httpContent = (HttpContent) msg;
@@ -44,7 +88,7 @@ public class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
 			System.out.println("body==============>" + buf.toString());
 			int i = httpReceiveTotal.incrementAndGet();
 			// 转发
-			String responseMessage = "false============>" + i +">>>>" + buf.toString();
+			String responseMessage = "false============>" + i + ">>>>" + buf.toString();
 			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
 			// Thread.sleep(10000L);
 			response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
@@ -54,7 +98,7 @@ public class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
 			buffer.release();
 			// 返回数据
 			ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-		}
+		}*/
 	}
 
 	@Override
