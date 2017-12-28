5e85a4660a46fa4c76e8c96efa27489fdca54a53
tupengxiong <511980432@qq.com>
2017-12-28 15:50:10 +0800

    kafka

--- kafka/src/main/java/com/lovesher/kafka/MyKafkaConsumer.java
+++ kafka/src/main/java/com/lovesher/kafka/MyKafkaConsumer.java
@@ -9,20 +9,20 @@
 
 package com.lovesher.kafka;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 /**
- * ClassName:MyKafkaConsumer <br/>
- * Function: TODO ADD FUNCTION. <br/>
- * Reason: TODO ADD REASON. <br/>
- * Date: 2017年12月26日 下午5:12:50 <br/>
+ * ClassName:MyKafkaConsumer br/>
+ * Function: TODO ADD FUNCTION. br/>
+ * Reason: TODO ADD REASON. br/>
+ * Date: 2017年12月26日 下午5:12:50 br/>
  * 
  * @author tupengxiong
  * @version
@@ -37,9 +37,8 @@ public class MyKafkaConsumer {
 		Properties props = new Properties();
 
 		// zookeeper 配置
-		props.put("zookeeper.connect", "192.168.0.120:2181");
 
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.120:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, MyKafkaProducer.bootstrapServers);
 		// 消费者所在组
 		props.put("group.id", "testgroup");
 
@@ -47,8 +46,10 @@ public class MyKafkaConsumer {
 		props.put("zookeeper.session.timeout.ms", "4000");
 		props.put("zookeeper.sync.time.ms", "200");
 		props.put("auto.commit.interval.ms", "1000");
-		props.put("auto.offset.reset", "earliest");
-
+		//props.put("auto.offset.reset", "earliest");
+		
+		//自动commit 也可手动控制  consumer.commitSync();
+		props.put("enable.auto.commit", "true");
 		// 序列化类
 		// 配置key的序列化类
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -58,12 +59,12 @@ public class MyKafkaConsumer {
 	}
 
 	void consume() {
-		List<String> topicCountMap = new ArrayList<String>();
-		topicCountMap.add(MyKafkaProducer.TOPIC);
-		consumer.subscribe(topicCountMap);
-		while (true) {
-			System.out.println(consumer.poll(10L).count());
-		}
+		 consumer.subscribe(Arrays.asList(MyKafkaProducer.TOPIC));
+		 while (true) {
+		     ConsumerRecords<String, String> records = consumer.poll(100);
+		     for (ConsumerRecord<String, String> record : records)
+		         System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+		 }
 	}
 
 	public static void main(String[] args) {
--- kafka/src/main/java/com/lovesher/kafka/MyKafkaProducer.java
+++ kafka/src/main/java/com/lovesher/kafka/MyKafkaProducer.java
@@ -22,46 +22,40 @@ public class MyKafkaProducer {
 
 	private final Producer<String, String> producer;
 	public final static String TOPIC = "test";
-
+	public final static String bootstrapServers = "192.168.0.120:9092";
+	public final static String ACKS = "all";
+	public final static String RETRIES = "1";
 	private MyKafkaProducer() {
 
 		Properties props = new Properties();
-		props.put("zookeeper.connect", "192.168.0.120:2181");
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "192.168.17.125");
 		// 此处配置的是kafka的broker地址:端口列表
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.120:9092");
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		// 配置key的序列化类
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
 		// 配置value的序列化类
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		
-		// request.required.acks
-		// 0, which means that the producer never waits for an acknowledgement
-		// from the broker (the same behavior as 0.7). This option provides the
-		// lowest latency but the weakest durability guarantees (some data will
-		// be lost when a server fails).
-		// 1, which means that the producer gets an acknowledgement after the
-		// leader replica has received the data. This option provides better
-		// durability as the client waits until the server acknowledges the
-		// request as successful (only messages that were written to the
-		// now-dead leader but not yet replicated will be lost).
-		// -1, which means that the producer gets an acknowledgement after all
-		// in-sync replicas have received the data. This option provides the
-		// best durability, we guarantee that no messages will be lost as long
-		// as at least one in sync replica remains.
-		props.put(ProducerConfig.ACKS_CONFIG, "0");
+		props.put(ProducerConfig.RETRIES_CONFIG, RETRIES);
+		/**
+		 * 0 不等待服务器接收 retries 设置不起作用
+		 * 1 部分接收
+		 * -1 all 全部接收
+		 */
+		props.put(ProducerConfig.ACKS_CONFIG, ACKS);
 
 		producer = new KafkaProducer<String, String>(props);
 	}
 
 	void produce() {
-		int messageNo = 1+1000;
-		final int COUNT = 101+messageNo;
+		int messageNo = 1;
+		final int COUNT = 101;
 
 		int messageCount = 0;
 		while (messageNo < COUNT) {
 			String key = String.valueOf(messageNo);
-			String data = "Hello kafka message :" + key;
+			String data = "Hello kafka message :" + key + "192.168.17.125";
 			producer.send(new ProducerRecord<String, String>(TOPIC, key, data));
 			System.out.println(data);
 			messageNo++;
@@ -69,6 +63,8 @@ public class MyKafkaProducer {
 		}
 
 		System.out.println("Producer端一共产生了" + messageCount + "条消息！");
+		
+		producer.close();
 	}
 
 	public static void main(String[] args) {
