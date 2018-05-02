ef4b14fb835e0852cf3737fe85a017e7576b54ea
tupengxiong <511980432@qq.com>
2018-05-02 18:34:54 +0800

    java proxy init

---
+++ proxy/src/main/java/com/lovesher/proxy/support/java/ExtendProxy.java
@@ -0,0 +1,31 @@
+/** 
+ * Project Name:proxy 
+ * File Name:ExtendProxy.java 
+ * Package Name:com.lovesher.proxy.support.java 
+ * Date:2018年5月2日下午6:31:34 
+ * Copyright (c) 2018, tupengxiong@qq.com All Rights Reserved. 
+ * 
+*/  
+  
+package com.lovesher.proxy.support.java;  
+/** 
+ * ClassName:ExtendProxy <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason:   TODO ADD REASON. <br/> 
+ * Date:     2018年5月2日 下午6:31:34 <br/> 
+ * @author   tupengxiong 
+ * @version   
+ * @since    JDK 1.7
+ * @see       
+ */
+public abstract class ExtendProxy {
+
+	
+	void doBefore() {
+	}
+	
+	
+	void doAfter() {
+	}
+}
+  
---
+++ proxy/src/main/java/com/lovesher/proxy/support/java/InvocationHandler.java
@@ -0,0 +1,66 @@
+/** 
+ * Project Name:proxy 
+ * File Name:InvocationHandler.java 
+ * Package Name:com.lovesher.proxy.support.java 
+ * Date:2018年5月2日下午6:28:57 
+ * Copyright (c) 2018, tupengxiong@qq.com All Rights Reserved. 
+ * 
+*/  
+  
+package com.lovesher.proxy.support.java;
+
+import java.lang.reflect.Method;
+
+/** 
+ * ClassName:InvocationHandler <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason:   TODO ADD REASON. <br/> 
+ * Date:     2018年5月2日 下午6:28:57 <br/> 
+ * @author   tupengxiong 
+ * @version   
+ * @since    JDK 1.7
+ * @see       
+ */
+public class InvocationHandler implements java.lang.reflect.InvocationHandler{
+
+	
+	private Object target;
+	
+	private ExtendProxy extend;
+	
+	public InvocationHandler(Object target) {
+		super();
+		this.target = target;
+	}
+	
+	public InvocationHandler(Object target, ExtendProxy extend) {
+		super();
+		this.target = target;
+		this.extend = extend;
+	}
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		if(null != extend){
+			extend.doBefore();
+		}
+		Object object = method.invoke(target, args);
+		if(null != extend){
+			extend.doAfter();
+		}
+		return object;
+	}
+	public Object getTarget() {
+		return target;
+	}
+	public void setTarget(Object target) {
+		this.target = target;
+	}
+	public Object getExtend() {
+		return extend;
+	}
+	public void setExtend(ExtendProxy extend) {
+		this.extend = extend;
+	}
+
+}
+  
