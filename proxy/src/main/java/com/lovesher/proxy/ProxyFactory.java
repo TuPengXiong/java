3009891735549c15b9cd9733f11623d70fc54bf2
tupengxiong <511980432@qq.com>
2018-05-02 18:24:57 +0800

    java proxy init

---
+++ proxy/src/main/java/com/lovesher/proxy/ProxyFactory.java
@@ -0,0 +1,24 @@
+/** 
+ * Project Name:proxy 
+ * File Name:ProxyFactory.java 
+ * Package Name:proxy 
+ * Date:2018年5月2日下午4:55:19 
+ * Copyright (c) 2018, tupengxiong@qq.com All Rights Reserved. 
+ * 
+*/  
+  
+package com.lovesher.proxy;  
+/** 
+ * ClassName:ProxyFactory <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason:   TODO ADD REASON. <br/> 
+ * Date:     2018年5月2日 下午4:55:19 <br/> 
+ * @author   tupengxiong 
+ * @version   
+ * @since    JDK 1.7
+ * @see       
+ */
+public class ProxyFactory {
+
+}
+  
